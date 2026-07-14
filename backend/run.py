a5ac35ff6816cbc1e998f2b066bc02c9db047536
josfam <jamukun@proton.me>
2026-07-14 14:34:32 +0300

    Add minimal run.py setup

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,12 @@
 """Entrypoint for the backend application."""
+
+from fastapi import FastAPI
+
+# The application object that represents the backend API "application"
+app = FastAPI(
+    title="encscribe backend",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
