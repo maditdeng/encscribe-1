ba93752951cf62f9c33abaa5e924804c9c411181
josfam <jamukun@proton.me>
2026-07-14 14:35:52 +0300

    Make the host more restrictive to just localhost

--- backend/run.py
+++ backend/run.py
@@ -9,4 +9,4 @@ app = FastAPI(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True)
