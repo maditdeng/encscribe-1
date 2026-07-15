32ecbb50392344b7a0e8fdac737a3db06eb1ad46
Madit Deng Achuil <maditdeng4@gmail.com>
2026-07-15 15:34:36 -0600

    first commit

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -10,7 +10,7 @@ function RootComponent() {
   return (
     <>
       <div className="bg-gray-200 gap-8 p-4 text-white font-bold flex items-center justify-center">
-        <p className="text-gray-950 text-2xl">Joseph's vault</p>
+        <p className="text-gray-950 text-2xl">Dido's vault</p>
         {/* button was here */}
         <div className="absolute right-10">
           <AddNoteButton />
