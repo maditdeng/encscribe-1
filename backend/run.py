95558029f1de4e76e2f29cb6d327f8359c9c6d02
Madit Deng Achuil <maditdeng4@gmail.com>
2026-07-21 08:33:47 -0600

    Add an api endpoint for /

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,14 @@
 """The entry point to the backend"""
+#cntrl + shift + p, type python interpreter
 from fastapi import FastAPI
 
 app = FastAPI(title="encscribe backend")
+
+# api endpoint for /
+@app.get("/")
+def root():
+    return {"status": "ok", "server load": 95} #dictionery
+
 
 if __name__ == "__main__":
     import uvicorn
