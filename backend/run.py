354dcb958232536ba71541c6ad668690f8b6cfc5
Madit Deng Achuil <maditdeng4@gmail.com>
2026-07-21 08:33:15 -0600

    Setup api application

--- backend/run.py
+++ backend/run.py
@@ -1,1 +1,8 @@
 """The entry point to the backend"""
+from fastapi import FastAPI
+
+app = FastAPI(title="encscribe backend")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True)
